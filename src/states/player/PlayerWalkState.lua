6f43588a453e35c6b81fef3499220ef095f2faa1
trashtiergames <hello@trashtiergames.com>
2023-03-26 21:07:47 +0300

    Fix some bugs and simplify

--- src/states/player/PlayerWalkState.lua
+++ src/states/player/PlayerWalkState.lua
@@ -3,24 +3,23 @@ PlayerWalkState = Class{__includes = BaseState}
 function PlayerWalkState:init(player, world)
   self.player = player
   self.world = world
-  self.direction = down
+  self.direction = "down"
+
+  -- Animations are solved in a slightly odd way because I saved each direction
+  -- as an individual file.
   self.img = {
-    ["up"] = love.graphics.newImage("art/player-walk-up"),
-    ["down"] = love.graphics.newImage("art/player-walk-down"),
-    ["left"] = love.graphics.newImage("art/player-walk-left"),
-    ["right"] = love.graphics.newImage("art/player-walk-right")
+    ["up"] = love.graphics.newImage("art/player-walk-up.png"),
+    ["down"] = love.graphics.newImage("art/player-walk-down.png"),
+    ["left"] = love.graphics.newImage("art/player-walk-left.png"),
+    ["right"] = love.graphics.newImage("art/player-walk-right.png")
   }
-  local grid = anim8.newGrid(32, 32, img.up:getWidth(), img.up:getHeight())
-  local dg = anim8.newGrid(32, 32, img.down:getWidth(), img.down:getHeight())
-  local lg = anim8.newGrid(32, 32, img.left:getWidth(), img.left:getHeight())
-  local rg = anim8.newGrid(32, 32, img.right:getWidth(), img.right:getHeight())
+  local grid = anim8.newGrid(16, 16, self.img.up:getWidth(), self.img.up:getHeight())
   self.animations = {
-    ["up"] = anim8.newAnimation(ug('1-4',1), 0.1),
-    ["down"] = anim8.newAnimation(dg('1-4',1), 0.1),
-    ["left"] = anim8.newAnimation(lg('1-4',1), 0.1),
-    ["right"] = anim8.newAnimation(rg('1-4',1), 0.1)
+    ["up"] = anim8.newAnimation(grid('1-4',1), 0.1),
+    ["down"] = anim8.newAnimation(grid('1-4',1), 0.1),
+    ["left"] = anim8.newAnimation(grid('1-4',1), 0.1),
+    ["right"] = anim8.newAnimation(grid('1-4',1), 0.1)
   }
-  self.animation = self.animations.down
 end
 
 function PlayerWalkState:update(dt)
@@ -103,25 +102,5 @@ function PlayerWalkState:update(dt)
 end
 
 function PlayerWalkState:render()
-  self.animation:draw(self.img[self.direction], self.x, self.y)
-end
-
-function PlayerWalkState:changeAnimation(dt, newDirection)
-  local animToUpdate = nil
-  if self.newDirection == "up" then
-    animToUpdate = self.upAnimation
-  elseif self.newDirection == "down" then
-    animToUpdate = self.downAnimation
-  elseif self.newDirection == "left" then
-    animToUpdate = self.leftAnimation
-  elseif self.newDirection == "right" then
-    animToUpdate = self.rightAnimation
-  end
-
-  if self.direction == newDirection then
-    animToUpdate:update(dt)
-  else
-    self.direction = newDirection
-    self
-
+  self.animation:draw(self.img[self.direction], self.player.x, self.player.y)
 end
